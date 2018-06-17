660926a4c76f4bcb004e8c7ac28f3324ad6a4f7a
Li Chen <chen1649chenli@gmail.com>
2018-06-17 09:20:50 -0500

    pass message use intent

--- MyMessenger/app/src/main/java/com/hfad/mymessenger/CreateMessengeActivity.java
+++ MyMessenger/app/src/main/java/com/hfad/mymessenger/CreateMessengeActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class CreateMessengeActivity extends AppCompatActivity {
 
@@ -14,7 +15,10 @@ public class CreateMessengeActivity extends AppCompatActivity {
     }
 
     public void onSendMessage(View view){
+        EditText messageView = (EditText) findViewById(R.id.message);
+        String messageText = messageView.getText().toString();
         Intent intent = new Intent(this, ReceiveMessageActivity.class);
+        intent.putExtra(ReceiveMessageActivity.EXTRA_MESSAGE, messageText);
         startActivity(intent);
 
     }
