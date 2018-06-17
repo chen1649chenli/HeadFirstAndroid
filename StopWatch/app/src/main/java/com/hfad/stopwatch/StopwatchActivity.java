58ed1f5a2aa362956bb00a4888047cc17835ff82
Li Chen <chen1649chenli@gmail.com>
2018-06-17 15:28:24 -0500

    implemented the runnable

--- StopWatch/app/src/main/java/com/hfad/stopwatch/StopwatchActivity.java
+++ StopWatch/app/src/main/java/com/hfad/stopwatch/StopwatchActivity.java
@@ -2,6 +2,7 @@ package com.hfad.stopwatch;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.TextView;
 
@@ -15,6 +16,7 @@ public class StopwatchActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stopwatch);
+        runTimer();
     }
 
     public void onClickStart(View view){
@@ -35,15 +37,20 @@ public class StopwatchActivity extends Activity {
 
     private void runTimer(){
         final TextView timeView = (TextView) findViewById(R.id.time_view);
-
-        int hours = seconds / 3600;
-        int minutes = (seconds%3600) / 60;
-        int secs = seconds % 60;
-        String time = String.format(Locale.getDefault(), "%d:%02d: %02d", hours, minutes, secs);
-        timeView.setText(time);
-        if(running){
-            seconds++;
-        }
-
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours = seconds / 3600;
+                int minutes = (seconds%3600) / 60;
+                int secs = seconds % 60;
+                String time = String.format(Locale.getDefault(), "%d:%02d: %02d", hours, minutes, secs);
+                timeView.setText(time);
+                if(running){
+                    seconds++;
+                }
+                handler.postDelayed(this, 1000);
+            }
+        });
     }
 }
